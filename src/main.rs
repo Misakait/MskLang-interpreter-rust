8812c5ef5578806560ef453f6d0d4ac983ca3fbd
misakajialin <cjl2597588144@gmail.com>
2025-06-22 22:16:02 +0800

    fix Scanning: Division operator & comments

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ fn main()  {
         "tokenize" => {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
@@ -42,7 +41,8 @@ fn main()  {
                         '-' => println!("MINUS - null"),
                         '/' => {
                             if chars.peek() == Some(&'/') {
-                                chars.next();
+                                chars.next(); // Consume the second slash
+                                while Some('\n') != chars.next(){}
                             } else {
                                 println!("SLASH / null");
                             }
